9493c2a82f97c6a0a67dd65c92af7d1ff7f25632
Utkarsh Bhimte <workwithutkarsh@gmail.com>
2025-02-28 15:39:18 -0700

    fix: for honmepage

---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+'use client';
+
+export default function HomePage() {
+    return (
+        <main className="flex min-h-screen min-w-full">
+            <div className="flex bg-privy-light-blue flex-1 p-6 justify-center items-center">
+                <div>
+                    <div className="mt-6 flex justify-center text-center">
+                        <h1>Welcome to Clank Insta</h1>
+                    </div>
+                </div>
+            </div>
+        </main>
+    );
+}
